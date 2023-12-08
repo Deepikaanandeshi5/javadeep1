6a730d36d93213725bd7bb39ff02b090fca6546b
deepika_a <deepika_a@HYL137117.photonint.com>
2023-12-08 13:22:13 +0530

    First commit

---
+++ Java Deep 1/src/practiceprograms/Program1.java
@@ -0,0 +1,11 @@
+ package practiceprograms;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Deepika");
+		System.out.println("Hello Welcome");
+
+	}
+
+}
